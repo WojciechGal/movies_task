cde146b3249a2278d5f56cb6543385e48c7e2d3b
Wojciech <wojciech.gladalski@gmail.com>
2020-09-05 11:43:02 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <RatingCalculator/>
+  </div>
+</template>
+
+<script>
+import RatingCalculator from "./components/RatingCalculator";
+
+export default {
+  name: 'App',
+  components: {
+    RatingCalculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/RatingCalculator.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div>Top Rated Movies Among Friends</div>
+        <div>Insert user ID:</div>
+        <label>
+            <input v-model="input"/>
+        </label>
+        <div v-if="topRatedList.length">
+            <ol>
+                <li v-for="movie in topRatedList" :key="movie">
+                    {{movie}}
+                </li>
+            </ol>
+        </div>
+        <div v-else>
+            No result found
+        </div>
+    </div>
+</template>
+
+<script>
+    import MoviesAnalyzer from "@/analyzers/MoviesAnalyzer";
+    export default {
+        name: "RatingCalculator",
+        data() {
+            return {
+                input: ''
+            }
+        },
+        computed: {
+            topRatedList: function () {
+                return MoviesAnalyzer.topRatedMoviesAmongFriends(parseInt(this.input))
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
